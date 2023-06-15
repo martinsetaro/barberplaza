a6866b8330e2afe82eab9152f335de981ac8706c
martinsetaro <lordsesony@gmail.com>
2023-06-15 18:35:26 -0300

    listo recuperar contraseña

--- src/app/restablecePassword/page.js
+++ src/app/restablecePassword/page.js
@@ -1,10 +1,122 @@
 'use client'
-import React from 'react'
+import React, { useEffect , useState }from 'react'
 import Link from 'next/link'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
+import { useRouter } from 'next/navigation'
 
 
 
 const page = () => {
+
+  const route = useRouter()
+  const [correoIngresado,setCorreoIngresado] = useState('')
+  const [password,setPassword] = useState('')
+  const [repetirPass,setRepetirPass] = useState('')
+  const [ datoFiltrado,setDatoFiltrado] = useState({})
+
+  useEffect(()=>{
+
+    function getEmailFromURL() {
+      const urlParams = new URLSearchParams(window.location.search);
+      return urlParams.get('email');
+    }
+
+    getEmailFromURL()
+    setCorreoIngresado(getEmailFromURL())
+   
+
+  },[])
+
+  //cambiar contraseña
+
+  console.log(typeof correoIngresado)
+
+  const handlerCambiarContraseña = () =>{
+     if([password,repetirPass].includes('')){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Algún campo esta vacio!',
+        timer:1500 
+      })
+      return;
+     }
+     if(password != repetirPass){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Las contraseñas no coinciden!',
+        timer:1500 
+      })
+      return;
+    }
+
+ buscarCorreo()
+
+  }
+        
+
+
+  async function actualizarCorreo(filtrado){
+     
+    const { id_infobarberias } = filtrado
+    
+    const url = `http://localhost:4001/barberias/${id_infobarberias}`;
+    await fetch(url,{
+      method:"PUT",
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({
+         pass:password
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if(data){
+        Swal.fire({
+          icon: 'success',
+          title: 'Hecho',
+          text: 'Contraseña actualizada!',
+          timer:1500 
+        })
+        route.push("/")
+      }
+    })
+    .catch(err => console.log(err))
+           
+     
+
+  }
+
+ 
+    async function buscarCorreo(){
+
+        const url= "http://localhost:4001/barberias"
+        await fetch(url,{
+          method:"GET"
+        })
+        .then(response => response.json())
+        .then(data => {
+            
+             const filtrado = data.find( item => item.correo === correoIngresado)
+             if(filtrado){
+              
+              actualizarCorreo(filtrado)
+             }
+        })
+        
+      .catch (error => {
+        console.log(error)})
+      }
+      
+     
+
+
+  
+    
+    
 
 
 
@@ -17,18 +129,24 @@ const page = () => {
         <div className="w-[40rem] h-auto p-4 m-auto shadow-lg rounded-md mt-8 bg-white flex flex-col">
             <h2 className='font-lora text-xl text-center mb-2'>Elige la nueva contraseña</h2>
             <h2 className='font-lora text-center mb-6'>Te solicitaremos nuevamente tu correo</h2>
-            <input 
+            <input
+            value={correoIngresado}
+            onChange={(correo) => setCorreo(correo)}
             className='border-2 border-slate-200 p-1 rounded-md mb-2'
             type="email" placeholder='Ingrese correo'/>
             <label>Nueva contraseña</label>
             <input
+            onChange={(e)=> setPassword(e.target.value)}
             className='border-2 border-slate-200 p-1 rounded-md mb-2'
             type="password" />
             <label>Repetir contraseña</label>
             <input
+            onChange={(e)=> setRepetirPass(e.target.value)}
             className='border-2 border-slate-200 p-1 rounded-md mb-2'
             type="password" />
-            <button className='mt-12 mb-4 bg-orange-600 m-auto text-white font-bold uppercase p-2 rounded-lg shadow-md w-[16rem]'>Solicitar nueva contraseña</button>
+            <button
+            onClick={handlerCambiarContraseña}
+            className='mt-12 mb-4 bg-orange-600 m-auto text-white font-bold uppercase p-2 rounded-lg shadow-md w-[16rem]'>Solicitar nueva contraseña</button>
         </div>
       
     </div>
