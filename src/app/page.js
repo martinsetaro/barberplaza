f84084b1e1eac0343c3891bb3747583fc2ae0495
martinsetaro <lordsesony@gmail.com>
2023-06-18 20:07:03 -0300

    modificando estilos para responsive

--- src/app/page.js
+++ src/app/page.js
@@ -14,14 +14,14 @@ export default function Home() {
       <div className=" min-[901px]:bg-white min-[901px]:bg-opacity-30 w-1/2 ml-0 min-h-screen max-[900px]:w-full flex flex-col items-center justify-center">
 
 
-     <Image className='w-[18rem] h-[12rem] mt-2' alt="logo" src={require('../../public/img/logooficial.png')}/>
+     <Image className='min-[1000px]:w-[18rem] mt-2 max-[900px]:w-[40rem] max-[900px]:h-[30rem] max-[900px]:-mt-32 ' alt="logo" src={require('../../public/img/logooficial.png')}/>
      <Link href="/Login"><button 
-     className=" bg-white text-2xl font-lora font-bold py-2 mb-4 mt-8 px-4 rounded-xl shadow-lg hover:transform hover:rotate-360 hover:scale-95 hover:duration-500 hover:bg-slate-600 hover:text-white">Ingresa a tu barberia</button></Link>
+     className=" bg-white text-2xl font-lora font-bold py-2 mb-4 mt-8 px-4 rounded-xl shadow-lg hover:transform hover:rotate-360 hover:scale-95 hover:duration-500 hover:bg-slate-600 hover:text-white max-[900px]:w-full max-[900px]:h-[9rem] max-[900px]:text-5xl">Ingresa a tu barberia</button></Link>
      <Link href="/Registro"><button 
-     className="bg-white text-2xl font-lora font-bold py-2 mb-8 mt-8 px-4 rounded-xl shadow-lg hover:transform hover:rotate-360 hover:scale-95 hover:duration-500 hover:bg-slate-600 hover:text-white">Registra tu barberia</button></Link>
+     className="bg-white text-2xl font-lora font-bold py-2 mb-8 mt-8 px-4 rounded-xl shadow-lg hover:transform hover:rotate-360 hover:scale-95 hover:duration-500 hover:bg-slate-600 hover:text-white max-[900px]:w-full max-[900px]:h-[9rem] max-[900px]:text-5xl">Registra tu barberia</button></Link>
      <Link href="/Turnos"><button 
-     className=" bg-blue-600 text-2xl text-white font-lora font-bold py-2 mb-4 mt-4 px-4 rounded-xl shadow-lg hover:transform hover:rotate-360 hover:scale-95 hover:duration-500 hover:bg-gradient-to-r from-blue-400 to-blue-600 hover:border-blue-700 hover:border-2 ">Solicitar un turno</button></Link>
-     <h3 className='mt-12 text-white font-lora font-bold'>Barber.V.1.00</h3>
+     className=" bg-blue-600 text-2xl text-white font-lora font-bold py-2 mb-4 mt-4 px-4 rounded-xl shadow-lg hover:transform hover:rotate-360 hover:scale-95 hover:duration-500 hover:bg-gradient-to-r from-blue-400 to-blue-600 hover:border-blue-700 hover:border-2 max-[900px]:w-full max-[900px]:h-[9rem] max-[900px]:text-5xl ">Solicitar un turno</button></Link>
+     <h3 className='mt-12 text-white font-lora font-bold max-[900px]:text-3xl'>Barber.V.1.00</h3>
      </div>
      
     </div>
