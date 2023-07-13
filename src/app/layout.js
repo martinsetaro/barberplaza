bf0322687db0a2beace858b0cd182eb8c37594a2
Martin Setaro <86411405+martinsetaro@users.noreply.github.com>
2023-07-13 17:34:40 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import Head from 'next/head';
+
 
 
 
@@ -14,22 +14,7 @@ export const metadata = {
 export default function RootLayout({ children}) {
   return (
     <html lang="en">
-    <Head>
-  <script
-    async
-    src="https://www.googletagmanager.com/gtag/js?id=G-XJQTMDLY80"
-  ></script>
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-XJQTMDLY80');
-      `,
-    }}
-  ></script>
-</Head>
+    
       <body
       suppressHydrationWarning={true} 
       >{children}</body>
